2da4d5e5a3da36ec444abdbebc0b22595b00d346
ady243 <masiviady7@gmail.com>
2024-06-03 17:04:16 +0200

    update cors

--- my-ci-cd/app/routes.php
+++ my-ci-cd/app/routes.php
@@ -10,10 +10,10 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
 return function (App $app) {
-    // $app->options('/{routes:.*}', function (Request $request, Response $response) {
-    //     // CORS Pre-Flight OPTIONS Request Handler
-    //     return $response;
-    // });
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
 
     $app->get('/', function (Request $request, Response $response) {
         $response->getBody()->write('Hello world!');
